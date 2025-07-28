8ba04059db53ebd50dc2e8142eabaa28f04df5e3
cardosoads <cardoso.tads@gmail.com>
2025-07-28 16:57:51 -0400

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,15 @@ declare(strict_types = 1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
+use Override;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    #[\Override]
+    #[Override]
     public function register(): void
     {
         //
@@ -22,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->setupLogViewer();
+    }
+
+    private function setupLogViewer(): void
+    {
+        LogViewer::auth(function ($request) {
+            return $request->user()?->is_admin;
+        });
     }
 }
